190a1a50f144584bdc6355af64983ecdd5e5cff4
abdukhalikovvv <jasurbek.abdukhalikov@mail.ru>
2020-07-26 19:39:58 +0500

    get negative nums

--- js/object.js
+++ js/object.js
@@ -1,1 +1,37 @@
-document.write('ercrc');
+// let book = {
+//     title : 'Shaytanat',
+//     author: 'Tohir Malik',
+//     id    : 1,
+//     price : '100$',
+// };
+// console.log(book);
+//
+// for (let informs in book){
+//     document.write(informs + '<br>');
+// }
+
+
+let nums = [1, 5, 8, -3, -6, -9];
+
+console.log(nums);
+
+// function getMinusNums(array) {
+//     return array.filter(function (value) {
+//         return value < 0;
+//     })
+// }
+//
+// console.log(getMinusNums(nums));
+
+function getNegativeNums(array) {
+    let negatives = [];
+
+    for (i = 0; i < array.length; i++){
+        if(array[i] < 0){
+            negatives.push(array[i]);
+        }
+    }
+    return negatives;
+}
+
+console.log(getNegativeNums(nums));
