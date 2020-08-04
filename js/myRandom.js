23714ef71b238f43e54f5d3e60aaf4ca96c0b421
abdukhalikovvv <jasurbek.abdukhalikov@mail.ru>
2020-08-04 12:26:07 +0500

    edited myRandom.js

--- js/myRandom.js
+++ js/myRandom.js
@@ -1,6 +1,6 @@
 let ranglar_soni = +prompt('Nechta rang kiritasiz');
 
-let ranglar = new Array();
+let ranglar = [];
 
 let box = document.querySelectorAll('.item');
 
@@ -12,12 +12,9 @@ for(i = 1; i <= ranglar_soni; i++){
 
 alert('siz istagan ranglar ' + ranglar);
 
-for (q = 0; q < box.length; q++){
-    let random = Math.floor(Math.random() * ranglar.length);
-
-    box[q].addEventListener('click', function () {
-        console.log(random);
-        this.style.background = ranglar[random];
+for (i = 0; i < box.length; i++){
+    box[i].addEventListener('click', function () {
+        this.style.background = ranglar[Math.floor(Math.random() * ranglar.length)];
     })
 }
 
